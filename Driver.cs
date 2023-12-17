d196424589eb0e10adfdde0f8636c28cf7ed8636
GeorgeMichailov10 <george@michailov.org>
2023-12-17 00:09:58 -0800

    Fixed Functionality and added Driver.

---
+++ Driver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical_Text_Extracter
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            string images_path = @"C:\path\to\tiff\files";
+            string temp_save_path = @"C:\path\to\temp\file\tmp.tif";
+            string excel_file_save_path = @"C:\path\to\save\excel\file\Classifications.xlsx";
+
+            try
+            {
+                // Process all TIFF images and save the results in an Excel file
+                Extractor.PutAllImagesIntoExcelDoc(images_path, excel_file_save_path, temp_save_path);
+                Console.WriteLine("All images have been processed and saved in the Excel file.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
--- TextExtract.cs
+++ TextExtract.cs
@@ -9,20 +9,20 @@ namespace Medical_Text_Extracter
 {
     public class Extractor
     {
+
+        private static TesseractEngine tesseractEngine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default);
+
         // Preconditions: image_path is the Absolute File Path to the image.
         // Postconditions: Returns a string array with all text on the image.
         public static string GetText(string image_path)
         {
             try
             {
-                using (TesseractEngine tess = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                using (Pix img = Pix.LoadFromFile(image_path))
                 {
-                    using (Pix img = Pix.LoadFromFile(image_path))
+                    using (Page page = tesseractEngine.Process(img))
                     {
-                        using (Page page = tess.Process(img))
-                        {
-                            return page.GetText();
-                        }
+                        return page.GetText();
                     }
                 }
             }
@@ -33,24 +33,43 @@ namespace Medical_Text_Extracter
             }
         }
 
-
         //
-        public static string GetEdgeText(string image_path, string top_image_path, string bottom_image_path)
+        public static string GetEdgeText(string image_path, string temp_save_path)
         {
-            if (!CreateCroppedImages(image_path, top_image_path, bottom_image_path))
+
+            string top_text = null;
+            string bottom_text = null;
+
+            try
             {
-                CleanUpCropping(top_image_path, bottom_image_path);
+                using (Bitmap origImage = new Bitmap(image_path))
+                {
+                    Rectangle cropAreaTop = new Rectangle((int)(origImage.Width * 0.1), 0, (int)(origImage.Width * 0.8), (int)(origImage.Height * 0.1));
+                    Rectangle cropAreaBottom = new Rectangle((int)(origImage.Width * 0.1), (int)(origImage.Height * 0.9), (int)(origImage.Width * 0.8), (int)(origImage.Height * 0.1));
+
+                    using (Bitmap croppedImageTop = origImage.Clone(cropAreaTop, origImage.PixelFormat))
+                    {
+                        croppedImageTop.Save(temp_save_path);
+                        top_text = GetText(temp_save_path);
+                        File.Delete(temp_save_path);
+                    }
+                    using (Bitmap croppedImageBottom = origImage.Clone(cropAreaBottom, origImage.PixelFormat))
+                    {
+                        croppedImageBottom.Save(temp_save_path);
+                        bottom_text = GetText(temp_save_path);
+                        File.Delete(temp_save_path);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
                 return null;
             }
-
-            string top_text = GetText(top_image_path);
-            string bottom_text = GetText(bottom_image_path);
-            CleanUpCropping(top_image_path, bottom_image_path);
 
             if (string.IsNullOrEmpty(top_text) && string.IsNullOrEmpty(bottom_text))
             {
                 string allText = GetText(image_path);
-                //// TODO: PROCESS TEXT IF YOU CAN
+                return allText;
             }
             else if (string.IsNullOrEmpty(top_text))
             {
@@ -62,7 +81,7 @@ namespace Medical_Text_Extracter
             }
             else
             {
-                
+                return null;
             }
         }
 
@@ -70,12 +89,14 @@ namespace Medical_Text_Extracter
 
         // Preconditions: Excel_File_Path is the correct file path to the .xlsm file, image_path is the absolute path to a singular image, classification is it's correct classification, row is greater than one, worksheet number is greater than or equal to one.
         // Postconditions: This will set the cells in specified row with the image path and it's classification on the corresponding worksheet of its .xlsm file.
-        public static void PutImageIntoExcelDoc(string Excel_File_path, string image_path, string classification, int row, int worksheet_number = 1) 
+        public static void PutImageIntoExcelDoc(string Excel_File_path, string image_path, string temp_save_path, int row = 1, int worksheet_number = 1) 
         {
-            using (XLWorkbook workbook = new XLWorkbook("File path to workbook")) 
+            using (XLWorkbook workbook = new XLWorkbook(Excel_File_path)) 
             {
                 if (worksheet_number < 1 || worksheet_number > workbook.Worksheets.Count) throw new ArgumentException("InvalidWorksheetNumberError");
                 var worksheet = workbook.Worksheet(worksheet_number);
+                if (row <= 1) row = worksheet.LastRowUsed().RowNumber() + 1;
+                string classification = GetEdgeText(image_path, temp_save_path);
                 worksheet.Cell(row, 1).Value = image_path;
                 worksheet.Cell(row, 2).Value = classification;
                 workbook.Save();
@@ -85,7 +106,7 @@ namespace Medical_Text_Extracter
 
         // Preconditions: file_path is the absolute file path in which all of the .tiff files are stored, ExcelFileSavePath is the path the user would like to use to save the newly created Excel file there.
         // Postconditions: A new Excel file will be created, being saved at the specified path. It will have two columns, one being the absolute file path to an image, and the second being a string representing its classification.
-        public static void PutAllImagesIntoExcelDoc(string file_path, string ExcelFileSavePath)
+        public static void PutAllImagesIntoExcelDoc(string file_path, string ExcelFileSavePath, string temp_save_path)
         {
             using(XLWorkbook workbook = new XLWorkbook())
             {
@@ -98,53 +119,21 @@ namespace Medical_Text_Extracter
                 string[] tiffFiles = Directory.GetFiles(file_path);
                 foreach (var tiffFile in tiffFiles)
                 {
-                    string classification = RemoveNoiseWords(GetText(tiffFile));
+                    string classification = GetEdgeText(tiffFile, temp_save_path);
                     worksheet.Cell(row, 1).Value = tiffFile;
                     worksheet.Cell(row, 2).Value = classification;
-                    workbook.Save();
                     ++row;
                 }
-                
+                workbook.Save();
             }
         }
 
-        // Preconditions: image_path is the absolute file path to an image, top_image_path and bottom_image_path are the locations where client wants croppings to be temporarily saved.
-        private static bool CreateCroppedImages(string image_path, string top_image_path, string bottom_image_path)
-        {
-            try
-            {
-                Bitmap origImage = new Bitmap(image_path);
-                Rectangle cropAreaTop = new Rectangle((int)(origImage.Width * 0.1), 0, (int)(origImage.Width * 0.8), (int)(origImage.Height * 0.1));
-                Rectangle cropAreaBottom = new Rectangle((int)(origImage.Width * 0.1), (int)(origImage.Height * 0.9), (int)(origImage.Width * 0.8), (int)(origImage.Height * 0.1));
 
-                Bitmap croppedImageTop = origImage.Clone(cropAreaTop, origImage.PixelFormat);
-                Bitmap croppedImageBottom = origImage.Clone(cropAreaBottom, origImage.PixelFormat);
-
-                croppedImageTop.Save(image_path);
-                croppedImageBottom.Save(image_path);
-                return true;
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return false;
-            }
-        }
         
-        // Preconditions: None.
-        // Postconditions: If files exist at those paths, it will delete them.
-        private static void CleanUpCropping(string top_image_path, string bottom_image_path)
-        {
-            try {       File.Delete(top_image_path);        }
-            catch (Exception e) {}
-            try {       File.Delete(bottom_image_path);        }
-            catch (Exception e) { }
-        }
    
         private static string ProcessText(string text)
         {
-
+            return "";
         }
 
         public static bool AlphaSort(string text_doc_file_path)
@@ -161,5 +150,7 @@ namespace Medical_Text_Extracter
                 return false;
             }
         }
+    
+        
     }
 }
